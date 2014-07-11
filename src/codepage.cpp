565506c2ccfefd8b4c5cd3285dec3be1137e2d4f
Kaan <kaanhas.52@gmail.com>
2014-07-11 23:58:31 +0300

    Revert "Source"
    This reverts commit 2001aa671a21ea832067d3529865e78e2a4dec01.

--- src/codepage.cpp
+++ src/codepage.cpp
@@ -1,7 +1,8 @@
-#include "Snake.h"
-#include "Field.h"
 #include <string>
 #include <sstream>
+#include <windows.h>
+#include "Snake.h"
+#include "Field.h"
 
 // MSVC Console close
 //#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
@@ -9,11 +10,9 @@
 using namespace std;
 
 int		time = 0;
-int		again = true;
-int		mainWindows;
-Field*	grid = NULL;
-Snake*	snake = NULL;
-Score*	highScore = NULL;
+Field	grid;
+Snake	snake;
+Score	highScore;
 
 /*
 *	Prototype functions
@@ -30,56 +29,31 @@ int main(int argc, char** argv)
 	// Console close..
 	FreeConsole();
 
-	do {
-		if( grid && snake && highScore )
-		{
-			time = 0;
-			delete snake;
-			delete highScore;
-			delete grid;
-		}
-		grid = new Field();
-		snake = new Snake();
-		highScore = new Score();
+	//	This function, is used to initialize the GLUT library.
+	glutInit(&argc, argv);
 
-		//	This function, is used to initialize the GLUT library.
-		glutInit(&argc, argv);
-
-		// This function, set the initial window size respectively.
-		glutInitWindowSize(WIDTH, HEIGHT);
-
-		// Windows start position
-		int x = GetSystemMetrics(SM_CXSCREEN) - WIDTH;
-		int y = GetSystemMetrics(SM_CYSCREEN) - HEIGHT;
-		glutInitWindowPosition(x/2, y/2);
+	// This function, set the initial window size respectively.
+	glutInitWindowSize(WIDTH, HEIGHT);
 	
-		// Setting the display mode
-		// glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	// Setting the display mode
+	// glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 	
-		// The title is given, and the window is created
-		mainWindows = glutCreateWindow("Snake 2D");
+	// The title is given, and the window is created
+	glutCreateWindow("Snake 2D");
 
-		// This function, sets the keyboard callback for the current window.
-		glutKeyboardFunc(keyboard);
-		glutSpecialFunc(specialKeys);
+	// This function, sets the keyboard callback for the current window.
+	glutKeyboardFunc(keyboard);
+	glutSpecialFunc(specialKeys);
 
-		InitializeGlutCallbacks();
-
-
-		// TThis function, sets the global idle callback.
-		//glutIdleFunc(display);
-
-		glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_CONTINUE_EXECUTION);
-
-		// This function, enters the GLUT event processing loop.
-		glutMainLoop();
-
-	} while( again );
+	InitializeGlutCallbacks();
 
 
-	delete snake;
-	delete highScore;
-	delete grid;
+	// TThis function, sets the global idle callback.
+	//glutIdleFunc(display);
+
+	// This function, enters the GLUT event processing loop.
+	glutMainLoop();
+
 
 	return EXIT_SUCCESS;
 }
@@ -97,29 +71,27 @@ static void renderScene()
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	// Draws the grid and score table
-	grid->draw();
-	grid->drawScoreTable();
+	grid.draw();
+	grid.drawScoreTable();
 
 	// Does a snake hits the ground?
-	if( !snake->collision( *highScore ) )
+	if(!snake.collision(highScore))
 	{
 		// 
-		snake->move();
+		snake.move();
 	}
 	else
 	{
 		// 
 		displayWrite(340.0f, 301.0f, GLUT_BITMAP_TIMES_ROMAN_24, "GAME OVER\0", Color(1.0f, 0.0f, 0.0f));
-		displayWrite(305.0f, 262.0f, GLUT_BITMAP_TIMES_ROMAN_24, "TRY AGAIN? (Y/N)\0", Color(1.0f, 0.0f, 0.0f));
 	}
-
 	// Snake draws
-	snake->draw();
+	snake.draw();
 
 	// Prints level and score
 	ostringstream outLvl, outScr;
-	outScr << highScore->getScore();
-	outLvl << highScore->getLevel();
+	outScr << highScore.getScore();
+	outLvl << highScore.getLevel();
 	string str = "LEVEL: " + outLvl.str() + "                     SCORE: " + outScr.str();
 	displayWrite( 220.0f, 565.0f, GLUT_BITMAP_TIMES_ROMAN_24, str.c_str(), Color(0.0f, 0.0f, 0.0f) );
 	
@@ -129,7 +101,7 @@ static void renderScene()
 	glFlush();
  
 	// This is a counter. But slowly move snake and regeneration of the program
-	int setSpeed = ( highScore->getLevel()*15 );
+	int setSpeed = ( highScore.getLevel()*15 );
 	setSpeed = (setSpeed <= 180) ? setSpeed : 180;
 	while( GetTickCount()-time <= 200-setSpeed );
 	time = GetTickCount();
@@ -148,7 +120,7 @@ static void InitializeGlutCallbacks()
 
 void specialKeys(int key, int x, int y)
 {
-	snake->specialKeys(key);
+	snake.specialKeys(key);
 }
 
 
@@ -164,24 +136,12 @@ void keyboard(unsigned char key, int x, int y)
 		case 'F':
 			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);	
 			break;
-		case 'y':
-		case 'Y':
-			again = true;
-			if( snake->collision(*highScore) ) glutLeaveMainLoop();
-			if( snake->collision(*highScore) ) glutDestroyWindow(mainWindows);
-			break;
-		case 'n':
-		case 'N':
-			again = false;
-			if( snake->collision(*highScore) ) glutLeaveMainLoop();
-			if( snake->collision(*highScore) ) glutDestroyWindow(mainWindows);
-			break;
 		case '\x1B':
 			exit(EXIT_SUCCESS);
 			break;
 	}
 
-	snake->normalKeys(key);
+	snake.normalKeys(key);
 }
 
 
